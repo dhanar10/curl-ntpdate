76c52781fb51249ce3e7b6840a5e690f8aabe7df
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2025-04-12 04:18:03 +0700

    feat: improve compatibility with old date command

--- curl-ntpdate.sh
+++ curl-ntpdate.sh
@@ -4,6 +4,6 @@
 # Based on https://unix.stackexchange.com/a/494688
 
 UTC_NOW=$(curl -I -s http://1.1.1.1 | grep "Date:" | cut -d " " -f 2-)
-UTC_NOW_ISO_8601=$(date -d "$UTC_NOW" -D "%a, %d %b %Y %T %Z" +'%Y-%m-%d %H:%M:%S')
+UTC_NOW_ISO_8601=$(TZ=UTC date -d "$UTC_NOW" +'%Y-%m-%d %H:%M:%S')
 date -u -s "$UTC_NOW_ISO_8601" > /dev/null
 date
